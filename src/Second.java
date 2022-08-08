f405ad148ed1c7b4384ee72974376daf09b33ecc
PunamBorude <borudepunam.24@gmail.com>
2022-08-08 15:45:05 +0530

    SecondCommit

--- src/Second.java
+++ src/Second.java
@@ -3,7 +3,7 @@ public class Second {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        // System.out.println("Secong program");
+         System.out.println("Second program");
 	}
 
 }
